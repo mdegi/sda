9c9995635538287b9551329c86338d9b8cea7535
Martin Degiorgio <martin.degiorgio@go.com.mt>
2020-10-22 15:24:27 +0200

    Adding project folders

---
+++ SDAController/src/main/java/com/md/sda/SDAApplicationLoader.java
@@ -0,0 +1,13 @@
+package com.md.sda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SDAApplicationLoader {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SDAApplicationLoader.class, args);
+	}
+
+}
---
+++ SDAController/src/test/java/com/md/sda/controller/SDAApplicationLoaderTests.java
@@ -0,0 +1,13 @@
+package com.md.sda.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SDAApplicationLoaderTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

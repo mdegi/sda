beb25df6ee39c6a9fd76c822fea0b5d27018fe30
Martin Degiorgio <martin.degiorgio@go.com.mt>
2021-03-20 19:28:56 +0100

    Added windows batch file

---
+++ mvnTest.bat
@@ -0,0 +1,4 @@
+set path=C:\Apps\openJDK\openjdk-11.0.10_9\bin;C:\Apps\apache-maven-3.6.3;C:\Apps\apache-maven-3.3.9\bin
+set JAVA_HOME=C:\Apps\openJDK\openjdk-11.0.10_9
+
+mvn -f C:\Academic\Other\git\sda\deploymentsManagementAPI\pom.xml clean test
